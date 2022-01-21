b3eaefeda50db131769876fec24213f7e52bca42
Misagh <mm1844@gmail.com>
2022-01-21 11:45:30 +0400

    create local etc/cas/ if not found

---
+++ app/src/main/java/org/apereo/cas/initializr/contrib/LocalEtcCasDirectoryContributor.java
@@ -0,0 +1,19 @@
+package org.apereo.cas.initializr.contrib;
+
+import io.spring.initializr.generator.project.contributor.ProjectContributor;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class LocalEtcCasDirectoryContributor implements ProjectContributor {
+    @Override
+    public void contribute(final Path projectRoot) throws IOException {
+        var output = projectRoot.resolve("etc/cas/.ignore");
+        if (!Files.exists(output)) {
+            Files.createDirectories(output.getParent());
+            Files.createFile(output);
+        }
+
+    }
+}
