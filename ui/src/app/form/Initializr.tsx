e7e1810c4293c2a988b8e43ae34f90b686e41c25
Ryan Mathis <rmathis@unicon.net>
2023-01-18 07:57:49 -0700

    Added sharing capability

--- ui/src/app/form/Initializr.tsx
+++ ui/src/app/form/Initializr.tsx
@@ -3,10 +3,12 @@ import { Backdrop, Button, CircularProgress, Divider, Grid } from '@mui/material
 import { useHotkeys } from "react-hotkeys-hook";
 import Customization from './Customization';
 import Dependencies from './Dependencies';
+import ShareOverlay from "./ShareOverlay";
+
 import { useApiLoaded, useVersionsLoaded } from '../store/AppReducer';
 import { Overlay } from '../data/Overlay';
 import { useCanDownload, useOverlay } from '../store/OverlayReducer';
-import { isNil, pickBy } from 'lodash';
+import { getOverlayFromQs, getOverlayQuery } from '../data/Url';
 import JSZip from "jszip";
 
 import * as FileSaver from 'file-saver';
@@ -18,6 +20,7 @@ import { Preview } from '../preview/Preview';
 import { Download } from '@mui/icons-material';
 import { useAppDispatch } from '../store/hooks';
 import { setPreviewSelected, setPreviewState, setPreviewTree } from '../store/PreviewReducer';
+import { setDependencies, setCustomization } from '../store/OverlayReducer';
 
 import { getTree } from "../file/tree";
 
@@ -38,8 +41,7 @@ export default function Initializr() {
     const [loading, setLoading] = React.useState(false);
 
     const fetchArchive = async (overlay: Overlay, type: string = 'tgz') => {
-        const used = pickBy(overlay, (value: any) => value !== "" && !isNil(value));
-        const string = qs.stringify(used, { arrayFormat: "comma" });
+        const string = getOverlayQuery(overlay);
         return await fetch(`${API_PATH}starter.${type}?${string}`);
     };
 
@@ -77,6 +79,12 @@ export default function Initializr() {
     useHotkeys("ctrl+space", () => explore(overlay), [overlay]);
     useHotkeys("ctrl+enter", () => download(overlay), [overlay]);
 
+    React.useEffect(() => {
+        const { dependencies, ...overlay } = getOverlayFromQs();
+        dispatch(setCustomization(overlay as Overlay));
+        dispatch(setDependencies(dependencies));
+    }, [])
+
     return (
         <Fragment>
             <Backdrop
@@ -102,7 +110,7 @@ export default function Initializr() {
                             <Customization />
                             <Divider style={{ margin: "1rem 0rem" }} />
                             <Grid container spacing={2}>
-                                <Grid item xs={6}>
+                                <Grid item xs={4}>
                                     <Button
                                         fullWidth
                                         variant="contained"
@@ -114,15 +122,26 @@ export default function Initializr() {
                                         Download
                                     </Button>
                                 </Grid>
-                                <Grid item xs={6}>
+                                <Grid item xs={4}>
                                     <Preview
                                         handlePreview={() => explore(overlay)}
                                         handleDownload={() => download(overlay)}
                                     />
                                 </Grid>
+                                <Grid item xs={4}>
+                                    <ShareOverlay
+                                        overlay={overlay}
+                                    />
+                                </Grid>
                             </Grid>
                             {loading && (
-                                <div style={{display: 'flex', justifyContent: 'center', marginTop: '2rem'}}>
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        marginTop: "2rem",
+                                    }}
+                                >
                                     <CircularProgress />
                                 </div>
                             )}
---
+++ ui/src/app/form/ShareOverlay.tsx
@@ -0,0 +1,39 @@
+import React, { Fragment } from "react";
+import {
+    Button,
+} from "@mui/material";
+import { Link } from "@mui/icons-material";
+import ShareOverlayDialog from "./ShareOverlayDialog";
+import { Overlay } from "../data/Overlay";
+import { getOverlayQuery } from "../data/Url";
+import { APP_PATH } from "../App.constant";
+
+export interface ShareOverlayProps {
+    overlay: Overlay
+}
+
+export default function ShareOverlay({ overlay }: ShareOverlayProps) {
+    const [open, setOpen] = React.useState(false);
+
+    const url = React.useMemo(
+        () => `${APP_PATH}?${getOverlayQuery(overlay)}`,
+        [overlay]
+    );
+
+    return (
+        <>
+            <Button
+                fullWidth
+                onClick={() => setOpen(true)}
+                variant="contained"
+                startIcon={<Link />}
+            >
+                Share
+            </Button>
+            <ShareOverlayDialog
+                url={url}
+                onClose={() => setOpen(false)}
+                open={open} />
+        </>
+    );
+}
