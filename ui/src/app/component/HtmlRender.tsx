5b54b1b3d93a782a1be83b16c12ce1b17387b43d
Ryan Mathis <rmathis@unicon.net>
2023-07-11 08:09:26 -0700

    Fixed sanitizer call

--- ui/src/app/component/HtmlRender.tsx
+++ ui/src/app/component/HtmlRender.tsx
@@ -5,10 +5,12 @@ import DOMPurify from 'dompurify';
 export function HtmlRender ({ html }: { html: string }) {
 
     const parsed = React.useMemo(() => {
-        return DOMPurify.sanitize(html);
+        let rootElement = document.createElement("div");
+        rootElement.appendChild(DOMPurify.sanitize(html, { RETURN_DOM_FRAGMENT: true }));
+        return rootElement.innerHTML;
     }, [html]);
 
     return (
-        <span dangerouslySetInnerHTML={{ __html: parsed }} />
+        <span dangerouslySetInnerHTML={{__html: parsed}} />
     );
 }
