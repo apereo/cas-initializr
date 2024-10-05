43c2448419c9d3efc483c085059e370c3c444930
Misagh Moayyed <mm1844@gmail.com>
2024-10-05 13:05:48 +0400

    support RCs when fetching modules

--- app/src/main/java/org/apereo/cas/initializr/metadata/CasOverlayInitializrMetadataFetcher.java
+++ app/src/main/java/org/apereo/cas/initializr/metadata/CasOverlayInitializrMetadataFetcher.java
@@ -1,12 +1,15 @@
 package org.apereo.cas.initializr.metadata;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import java.util.Comparator;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 public class CasOverlayInitializrMetadataFetcher implements InitializrMetadataFetcher {
     private final MongoTemplate mongoTemplate;
 
@@ -14,7 +17,12 @@ public class CasOverlayInitializrMetadataFetcher implements InitializrMetadataFe
     @Override
     public List<CasDependency> fetch(final String casVersion) {
         try {
-            var collection = "casmodules" + casVersion.replace("-SNAPSHOT", "").replace(".", "");
+            val versionToUse = casVersion.replace("-SNAPSHOT", "")
+                .replace(".", "")
+                .replaceFirst("-RC\\d*", "");
+            
+            var collection = "casmodules" + versionToUse;
+            log.info("Fetching CAS modules from collection {} for {}", collection, casVersion);
             var allDependencies = mongoTemplate.findAll(CasDependency.class, collection);
             allDependencies.sort(Comparator.comparing(o -> o.getDetails().getCategory()));
             return allDependencies;
