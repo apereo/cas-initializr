88fd7d0ff4bf0b2857ce6e542d48ff97c4e80587
Misagh Moayyed <mm1844@gmail.com>
2024-11-17 12:20:00 +0400

    fetch modules from previous collection if unavailable

--- app/src/main/java/org/apereo/cas/initializr/metadata/CasOverlayInitializrMetadataFetcher.java
+++ app/src/main/java/org/apereo/cas/initializr/metadata/CasOverlayInitializrMetadataFetcher.java
@@ -1,8 +1,8 @@
 package org.apereo.cas.initializr.metadata;
 
+import org.apereo.cas.initializr.web.VersionUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import java.util.Comparator;
@@ -17,17 +17,33 @@ public class CasOverlayInitializrMetadataFetcher implements InitializrMetadataFe
     @Override
     public List<CasDependency> fetch(final String casVersion) {
         try {
-            val versionToUse = casVersion.replace("-SNAPSHOT", "")
-                .replace(".", "")
-                .replaceFirst("-RC\\d*", "");
-            
-            var collection = "casmodules" + versionToUse;
-            log.info("Fetching CAS modules from collection {} for {}", collection, casVersion);
-            var allDependencies = mongoTemplate.findAll(CasDependency.class, collection);
+            var parsedVersion = VersionUtils.parse(casVersion);
+            var versionToUse = determineVersionToUse(casVersion);
+            var allDependencies = getFetchDependencies(versionToUse);
+            var count = 0;
+            while (count < 2 && allDependencies.isEmpty()) {
+                log.warn("No CAS modules found for {}", casVersion);
+                count++;
+                versionToUse = parsedVersion.getMajor() + "" + parsedVersion.getMinor() + "" + (parsedVersion.getPatch() - count);
+                allDependencies = getFetchDependencies(versionToUse);
+            }
             allDependencies.sort(Comparator.comparing(o -> o.getDetails().getCategory()));
             return allDependencies;
         } catch (final Exception e) {
             throw new RuntimeException(e.getMessage(), e);
         }
+    }
+
+    private List<CasDependency> getFetchDependencies(final String versionToUse) {
+        var collection = "casmodules" + versionToUse;
+        log.info("Fetching CAS modules from collection {} for {}", collection, versionToUse);
+        return mongoTemplate.findAll(CasDependency.class, collection);
+    }
+
+    private String determineVersionToUse(final String casVersion) {
+        return casVersion.replace("-SNAPSHOT", "")
+            .replace(".", "")
+            .replaceFirst("-RC\\d*", "")
+            .replaceFirst("-M\\d*", "");
     }
 }
