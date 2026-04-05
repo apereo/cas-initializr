c2d226455be5c476cb655d4f2774e184b2bd594a
Misagh Moayyed <mm1844@gmail.com>
2026-04-05 10:58:19 -0700

    fix TS6 build issues

--- ui/src/react-app-env.d.ts
+++ ui/src/react-app-env.d.ts
@@ -1,1 +1,35 @@
 /// <reference types="react-scripts" />
+
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+  export { ReactComponent };
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
